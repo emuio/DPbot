6ad4d862346cd4193ccdc60db63f358462d8863f
dpyyds <729831790@qq.com>
2025-04-29 11:38:55 +0800

    DPBOT来了，只为复活NGCBOT

--- DPBOT_docker/app/config.py
+++ DPBOT_docker/app/config.py
@@ -15,7 +15,7 @@ WXAPI_PORT = int(os.environ.get("WXAPI_PORT", 8057))
 WXAPI_URL = f"http://{WXAPI_HOST}:{WXAPI_PORT}"  #docker
 #WXAPI_URL = f"http://localhost:8057" #本地测试
 SELFWXID = os.environ.get("WXID", "wxid_yby6o1jbfqyd12")# 机器人的wxid 换成你自己的啊
-#WXAPI_WS_URL = f"ws://{WXAPI_HOST}:{WXAPI_WS_PORT}/ws"   #docker
+WXAPI_WS_URL = f"ws://{WXAPI_HOST}:{WXAPI_WS_PORT}/ws"   #docker
 # 用户配置
 DEFAULT_WXID = os.environ.get("WXID", "wxid_yby6o1jbfqyd12")# 机器人的wxid 换成你自己的啊
 
